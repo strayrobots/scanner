f1ff092a8143e4e60b7c94315d21c55fcd1d61b2
Kenneth Blomqvist <kekeblom@gmail.com>
2020-11-15 15:14:14 +0100

    Initial Commit

---
+++ Stray ScannerTests/Stray_ScannerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Stray_ScannerTests.swift
+//  Stray ScannerTests
+//
+//  Created by Kenneth Blomqvist on 11/15/20.
+//  Copyright © 2020 Stray Robots. All rights reserved.
+//
+
+import XCTest
+@testable import Stray_Scanner
+
+class Stray_ScannerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
