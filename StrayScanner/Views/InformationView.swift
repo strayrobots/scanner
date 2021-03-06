bd7eb73566118c3e4887da4f415d2701246f99ab
Kenneth Blomqvist <kekeblom@gmail.com>
2021-03-06 22:57:59 +0100

    Update about page

--- StrayScanner/Views/InformationView.swift
+++ StrayScanner/Views/InformationView.swift
@@ -18,7 +18,7 @@ struct InformationView: View {
             VStack(alignment: .leading) {
                 Text("About This App").font(.title)
                     .fontWeight(.bold)
-                    
+                Group {
                 bodyText("""
 This app lets you record video and depth datasets using the camera and LIDAR scanner.
 """)
@@ -26,31 +26,36 @@ This app lets you record video and depth datasets using the camera and LIDAR sca
                 heading("Transfering Datasets To Your Desktop")
                 
                 bodyText("""
-The recorded datasets can be transfered to your desktop computer by connecting your device to it with the lightning cable.
+The recorded datasets can be transferred to your desktop computer by connecting your device to it with the lightning cable.
 
 On Mac, you can access the files through Finder. In the sidebar, select your device. Under the "Files" tab, you should see an entry for StrayScanner. Expand it, then drag the folders to the desired location. There is one folder per dataset, each named after a random alphanumerical hash.
 
 On Windows, you should be able to access the files through iTunes.
 """)
-                
+
                 heading("Using The Data")
                 
-                bodyText("Below is a link to a Python script that uses Open3D to visualize the collected data. It contains a detailed description of the semantics for each attribute.")
+                bodyText("Below is a Python script which uses Open3D to make a 3D reconstruction of the scene. Here is also a detailed description of the data model.")
                 
-                link(text: "Usage example", destination: "https://keke.dev")
-                
-                heading("Disclaimer")
-                
-                bodyText("""
-This application is provided as is.
+                link(text: "Example script", destination: "https://gist.github.com/kekeblom/f0e7925d514db3c733df8bc30dc1ff4b")
+                link(text: "Data model", destination: "https://www.notion.so/Stray-Scanner-Data-Model-aea95b702b484d41b2de12d2e90781ce")
+                    
+                }
+                Group {
+                heading("Privacy Policy")
 
-In no event shall the authors or copyright holders be liable for any claim, damages or other liability arising from using, or in connection with using the software.
-""")
+                bodyText("We do not track you. All of the data you record is stored on your device. We never call home or otherwise collect data about how you use the app.")
+
+                link(text: "Privacy policy", destination: "https://www.notion.so/Privacy-Policy-f1a6b1bcf7ed48098ffe2f50281e5c34")
+
+                heading("Disclaimer")
+                bodyText("This application is provided as is.\nIn no event shall the authors or copyright holders be liable for any claim, damages or other liability arising from using, or in connection with using the software.")
+                
                 Spacer()
+                }
             }.padding(.all, paddingLeftRight)
             }
             .frame(minWidth: 0, maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
-
         
         }
     }
